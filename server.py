f92920377baee15f8c4922be22cf54a192d702e3
Ivan Velo Castaneda <iavelocastaneda@mail.fhsu.edu>
2024-09-25 19:03:53 -0500

    Created a client/server structure

--- server.py
+++ server.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+# Serve the index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('message')
-def handleMessage(msg):
-    print('Message: ' + msg)
-    send(msg, broadcast=True)
+# Handle draw events sent from clients
+@socketio.on('draw')
+def handle_draw(data):
+    # Log the drawing data received from a client
+    print(f'Received drawing data: {data}')
+    
+    # Broadcast the drawing data to all clients (including the sender)
+    emit('draw', data, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- static/js/whiteboard.js
+++ static/js/whiteboard.js
@@ -1,24 +1,30 @@
+// Initialize socket.io connection
+var socket = io();
+
+// Get canvas and set up context for drawing
 var canvas = document.getElementById("whiteboard");
 var context = canvas.getContext("2d");
 var drawing = false;
 
-var socket = io();
-
 // Start drawing
 canvas.addEventListener("mousedown", function (e) {
   drawing = true;
+  context.beginPath();
   context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
 });
 
-// Draw and emit draw events
+// Send drawing data to server on mouse movement
 canvas.addEventListener("mousemove", function (e) {
   if (drawing) {
-    var data = {
+    var drawData = {
       x: e.clientX - canvas.offsetLeft,
       y: e.clientY - canvas.offsetTop
     };
-    socket.emit("draw", data);
-    context.lineTo(data.x, data.y);
+    // Send drawing data to server
+    socket.emit("draw", drawData);
+
+    // Draw locally on the client's canvas
+    context.lineTo(drawData.x, drawData.y);
     context.stroke();
   }
 });
@@ -28,13 +34,9 @@ canvas.addEventListener("mouseup", function () {
   drawing = false;
 });
 
-// Handle incoming drawing events from other users
+// Listen for draw events from the server
 socket.on("draw", function (data) {
+  // Update the canvas with drawing data from the server
   context.lineTo(data.x, data.y);
   context.stroke();
-});
-
-// Stop drawing if mouse leaves canvas
-canvas.addEventListener("mouseleave", function () {
-  drawing = false;
 });
